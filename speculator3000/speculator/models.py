c045f86fcd0f2305339ebaa8249f270f27ec71a4
florian-bizet <sunburnstar84@gmail.com>
2024-12-15 23:25:51 +0100

    Card class model

--- speculator3000/speculator/models.py
+++ speculator3000/speculator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# Card class
+class Card(models.Model):
+    cardName = models.CharField(max_length=100)
+    cardRarity = models.CharField(max_length=50)
+    cardSeries = models.CharField(max_length=100)
+    cardIndex = models.IntegerField("Index in its series (ex 079/121)")
+    cardImage = models.URLField("URL which leads to an image of this card")
+
+    def __str__(self):
+        return "{} {} {} from {} series".format(self.cardRarity, self.cardName, self.cardSeries, self.cardIndex)
+    
+
+
+
+
