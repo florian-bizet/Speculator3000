69d20f797daa79141357c5b9593b88ab22316bad
florian-bizet <sunburnstar84@gmail.com>
2024-12-16 00:07:15 +0100

    DB

--- speculator3000/speculator/models.py
+++ speculator3000/speculator/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# Extension class
+class Extension(models.Model):
+    extensionName = models.CharField(max_length=100)
+    extensionReleaseDate = models.DateField()
+    
+
 # Card class
 class Card(models.Model):
     cardName = models.CharField(max_length=100)
     cardRarity = models.CharField(max_length=50)
-    cardSeries = models.CharField(max_length=100)
+    cardExtension = models.ForeignKey(Extension, on_delete=models.CASCADE)
     cardIndex = models.IntegerField("Index in its series (ex 079/121)")
     cardImage = models.URLField("URL which leads to an image of this card")
 
     def __str__(self):
-        return "{} {} {} from {} series".format(self.cardRarity, self.cardName, self.cardSeries, self.cardIndex)
+        return "{} {} {} from {} series".format(self.cardRarity, self.cardName, self.cardExtension.extensionName, self.cardIndex)
     
 
 
